dae187f3c53bb91bc79ffaaabc0c27aee30b2d52
tanshilin <oppshorer@gmail.com>
2022-09-20 23:58:33 +0800

    1

--- docs/set-env.sh
+++ docs/set-env.sh
@@ -1,2 +1,4 @@
 set HTTP_PROXY="http://127.0.0.1:58895"  HTTPS_PROXY="http://127.0.0.1:58895" ENVIRONMENT
-update terminal to use bash.exe
+# update terminal to use bash.exe
+update file encoding to utf8
+update code style line-separator to unix
